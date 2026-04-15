031e1d74bd297016f5263e7e26d475e199a46c7b
live <eg.guilherme0309@gmail.com>
2026-04-15 09:50:12 -0300

    cap5 add

--- cap3/exemplo01.py
+++ cap3/exemplo01.py
@@ -1,3 +1,14 @@
-bicycles = ['trek', 'cannondale', 'redline', 'specialized']
-message = "My first bicycle was a " + bicycles[0].title() + "."
-print(message)
+
+# Lista inicial
+convidados = ['arthur', 'bruno', 'caio', 'diego']
+
+# Alteracao
+convidados.remove("arthur")
+convidados.append("thiago")
+
+
+while(len(convidados)>2):
+    print(f"Sinto muito, mas voce nao podera vir{convidados.pop()}")  
+
+for name in convidados:
+    print(f"Venha para minha festa {name}")
---
+++ cap4/tuplas_01.py
@@ -0,0 +1,9 @@
+# Tupla eh uma lista de itens que não poderá mudar.
+# Python refere-se a valores que não podem mudar como imutáveis.
+# Uma lista imutável é chamada de tupla.
+
+
+# Definindo uma tupla:
+retangulo = (200,50)
+print(retangulo[0])
+print(retangulo[1])
